421d2c06dd7111593ce71f970a5b0eba38b49aec
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-14 23:58:21 +0300

    update based on task 5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Task 5. Mock logging in"""
+
+from flask import g, Flask, render_template, request
+from flask_babel import Babel  # type: ignore
+from typing import Union
+
+app = Flask(__name__)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """Config class for Babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+def get_user(user_id: str) -> Union[dict, None]:
+    """Get user dictionary"""
+    if not user_id.isnumeric():
+        return None
+
+    return users.get(int(user_id), None)
+
+
+@app.before_request
+def before_request():
+    """Get user from url and set it as global."""
+    user_id = request.args.get('login_as')
+    if user_id:
+        g.user = get_user(user_id)
+    else:
+        g.user = None
+
+
+@babel.localeselector
+def get_locale():
+    """Locale selector based on user preferences."""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def default_route():
+    """Default route, Hello world"""
+    if g.user:
+        username = g.user['name']
+    return render_template('5-index.html', username=username)
+
+
+if __name__ == "__main__":
+    app.run()
--- 0x02-i18n/README.md
+++ 0x02-i18n/README.md
@@ -1,3 +1,37 @@
 # Flask Babel
 
 Add internalization support to flask application.
+
+## Procedure
+
+1. Extract text to be translated and add them to `.pot` file
+
+    `$ pybabel extract -F babel.cfg -o messages.pot dir`
+
+    *-F*: config files containing regex for files to be checked\
+    *-o*: output file\
+    *dir*: directory within which to check for files
+
+2. Create edittable files(.po) for each language
+
+    `$ pybabel init -i messages.pot -d translations -l en`
+
+    *-i*: .pot file to use\
+    *-d*: directory to put the files\
+    *-l*: language
+
+3. Add the translations in the files `.po` in the created translations folder
+
+4. Compile the files (`.po` -> `.mo`)
+
+   `$ pybabel compile -d translations`
+
+   *-d*: folder with the `.po` files
+
+*PS:* If the strings change, create a new `.pot` file and run below to merge the changes
+`$ pybabel update -i messages.pot -d translations`
+
+## Common errors
+
+`jinja2.ext has no attribute autoescape`
+[Solution]: pip3 install 'jinja<3.1.0'
