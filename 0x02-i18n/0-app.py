11e7dbc6ac671c044dffc3261e0693c502defc66
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-14 04:44:02 +0300

    update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def default_route():
-    """Default route, hello world"""
+    """Default route, Hello world"""
     return render_template('0-index.html')
 
 
