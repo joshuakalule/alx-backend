cd8baa67d6e990b041a58669f52d789520063fa9
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-14 05:17:23 +0300

    add files localeselector from best match

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Task 0.basic Flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel  # type: ignore
+
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for Babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Locale selector based on user preferences."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def default_route():
+    """Default route, Hello world"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
