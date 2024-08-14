887764ae6539c728b1b83aaa8d9105741efc691d
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-14 22:10:44 +0300

    add messages.pot

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Task 3. Parametrize templates"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _  # type: ignore
+
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for Babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Locale selector based on user preferences."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def default_route():
+    """Default route, Hello world"""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
