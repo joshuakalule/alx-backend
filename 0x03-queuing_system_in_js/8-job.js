dd2c8b43a40894b94dbd8c016129f6e236b9266a
Joshua Kalule <kaljoshkalule@gmail.com>
2024-09-24 00:38:18 +0300

    update files

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,5 +1,23 @@
 function createPushNotificationsJobs(jobs, queue) {
   if (!Array.isArray(jobs)) {
-    throw new Error()
+    throw new Error('Jobs is not an array');
+  }
+  for (const jobObj in jobs) {
+    const job = queue.create('push_notification_code_3', jobObj);
+    job
+      .on('enqueue', () => {
+        console.log(`Notification job created: ${job.id}`);
+      })
+      .on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+      })
+      .on('failed', (err) => {
+        console.log(`Notification job ${job.id} failed: ${err.message ?? err.toString()}`);
+      })
+      .on('progress', (progress, _data) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      });
+    job.save();
   }
 }
+export default createPushNotificationsJobs;
